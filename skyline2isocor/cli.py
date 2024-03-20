bfe13aa81ed217400d4d5e2840444ba14842e130
llegregam <legregam@insa-toulouse.fr>
2024-03-20 13:48:17 +0100

    Changed input data format to tabular

--- skyline2isocor/cli.py
+++ skyline2isocor/cli.py
@@ -23,17 +23,34 @@ def parse_args():
 def _get_isotopologue_number(row):
 
     original = row["isotopologue"]
-    if original == "[M-H]":
+    if original == "[M-H]" or original == "[M+H]":
         row["isotopologue"] = "0"
         return row
     row["isotopologue"] = original[2:].split("C")[0]
     return row
 
+def validate_file_extension(path):
+    """
+    Ensure file extension is of expected type
+    :param path: input pathlib path object
+    :return:
+    """
+
+    if not path.exists():
+        raise ValueError("Input path is not valid")
+    ext = path.suffix
+    if ext not in [".dat", ".tsv", ".txt"]:
+        raise ValueError(f"Error in file extension. File should be of tabular format or .dat. Detected format: {ext}")
+    return ext
+
 def process(args):
 
     try:
         input_path = Path(args.input)
-        data = pd.read_csv(str(input_path), sep=";")
+        file_ext = validate_file_extension(input_path)
+        if not file_ext:
+            raise ValueError("Error in file extension. File should be of tabular format or .dat")
+        data = pd.read_csv(str(input_path), sep="\t")
     except Exception:
         print(f"There was an error while reading the data. Data path: {args.input}")
         raise
@@ -50,7 +67,7 @@ def process(args):
     }
     isocor_data = data.rename(column_mapping, axis=1).drop("Product Mz", axis=1)
     isocor_data.insert(loc=2, column="derivative", value="")
-    isocor_data = isocor_data.apply(_get_isotopologue_number, axis=1)
+    isocor_data = isocor_data.apply(func=_get_isotopologue_number, axis=1)
     isocor_data = isocor_data.fillna(0)
     print(f"Final dataframe:\n{isocor_data}")
     isocor_data.to_csv(args.output, sep="\t", index=False)
